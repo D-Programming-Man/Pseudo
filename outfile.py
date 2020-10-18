eaac509b77022108e68771dff6d8e1cfe232fb2c
D-Programming-Man <40450924+D-Programming-Man@users.noreply.github.com>
2020-10-18 16:04:29 -0700

    Updated test files

--- outfile.py
+++ outfile.py
@@ -1,19 +1,22 @@
 if __name__ == "__main__":
   x = 2
   x = 4879438728978942759842
-  x = 1.431
+  q = 1.431
   x = "Hello_World"
-  x = "Hello World"
-  x = "FJEOIEE OFNE OIFN OEINFOSE FOE SF"
-  x = 2
+  d = "Hello World"
+  a = "FJEOIEE OFNE OIFN OEINFOSE FOE SF"
+  t = 2
   y = x
-  y = y
+  z = y
   print("Hello")
   print("Hello World")
   print("Hello" + str(x))
   print("Hello World, and hello to this world too " + str(x))
   print(str(x) + str(y))
-  print(str(x) + "' and '" + str(y))
+  print(str(x))
   print("Long" + "Boy")
-  print("This" + "Is" + "A" + "String"x" Yeet!")
-  print("The above line is a bug, going to fix this later though.")
+  print("This" + "Is" + "A" + "String\"x\" Yeet!")
+  print("Display now supports escape characters for quotes: \"")
+  print(str(1))
+  print(str(1.12323))
+  print(str(1) + str(123.2323))
