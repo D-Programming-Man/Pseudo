c739d3020789f0a7713c324ec9136c142523231f
D-Programming-Man <40450924+D-Programming-Man@users.noreply.github.com>
2020-10-27 00:05:19 -0700

    Check empty pseudo files and modify outputs
    Made the interpreter handle errors for empty files and fixed how the outputs should display to the console of the GUI. Will probably need to update the code so that it is more optimized. Can be easily changed to a function by switching out the comments near the main function declaration.

--- interpreter.py
+++ interpreter.py
@@ -1,13 +1,22 @@
 import sys
+import os
 from interlib import create
 from interlib import display
 from interlib import arithmetic
 
 if __name__ == "__main__":
+#def interpret(pseudo_file, python_file):
   # This file will later become a function to call, but for now let's just assume that this is the main function that takes in these files in the same directory
-  in_file = open("test.pseudo", "r")
-  py_file = open("outfile.py", "w")
+  pseudo_file = "test.pseudo"
+  python_file = "outfile.py"
+  in_file = open(pseudo_file, "r")
+  py_file = open(python_file, "w")
+  default_stdout = sys.stdout
+  output_file = open('output.txt', 'w')
+  sys.stdout = output_file
   
+  if (os.stat(pseudo_file).st_size == 0):
+    print("Error: Pseudo file emtpy. Did you properly saved it first?")
   
   # All varaibles, list, and dictionaries are put into here from the "Create" keyword
   # Key = variable name, Values = {data type, data for that variable name}
@@ -25,7 +34,7 @@ if __name__ == "__main__":
   # Some initialization to write to the py_file
   py_lines.append('if __name__ == "__main__":\n')
   indent = 2
-  parse_success = True
+  parse_success = False
   
   # main loop to parse all words in the file
   for line in in_file:
@@ -92,10 +101,27 @@ if __name__ == "__main__":
   py_file.close()
   
   # Runs the output file, stores output into output.txt file
-  default_stdout = sys.stdout
-  output_file = open('output.txt', 'w')
-  sys.stdout = output_file
-  exec(open('outfile.py').read())
+  py_cmds = ""
+  py_line_main_pos = 0
+  
+  # Kind of stupid, but apparently the output.txt file will not
+  # generate the outputs because of the if __name__ == "__main__"
+  # line. So we need to omit this and then omit the two space
+  # indentation for all lines after it.
+  for line in py_lines:
+    if line == 'if __name__ == "__main__":\n':
+      break
+    py_line_main_pos += 1
+    
+  py_lines.remove('if __name__ == "__main__":\n')
+  for i in range(0, len(py_lines)):
+    if i < py_line_main_pos:
+      py_cmds += py_lines[i]
+    else:
+      py_cmds += py_lines[i][2:]
+  
+  # Print the outputs to the output.txt file
+  exec(py_cmds)
   output_file.close()
   sys.stdout = default_stdout
   
