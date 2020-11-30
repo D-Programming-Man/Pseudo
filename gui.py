fb59de438ef91e93c34a29fe986155a27b573084
D-Programming-Man <40450924+D-Programming-Man@users.noreply.github.com>
2020-11-30 10:49:27 -0800

    Added a thing to detect closing of the GUI
    Added a feature where when a user closes the GUI, the whole program closes.
    Needed because the "Live Update" feature would still have a reference to the loaded file and if the GUI closes then the input text would be all cleared. However, this doesn't mean that the "Live Update" stops terminating and the opened file will be overwritten, clearing the file of its contents. This protocol stops this.

--- gui.py
+++ gui.py
@@ -1,7 +1,15 @@
 import tkinter as tk
 from guilib.application import Application
-         
+from interpreter import interpret
+import sys
+
 if __name__ == "__main__":
    root = tk.Tk()
    app = Application(master=root)
+   
+   # When user presses the X button to close GUI
+   def close():
+      sys.exit(0)
+   root.protocol("WM_DELETE_WINDOW", close)
+   
    app.mainloop()
