e27bc26da8a4ff1bb18989c043912cd296dd518f
D-Programming-Man <40450924+D-Programming-Man@users.noreply.github.com>
2020-12-01 21:51:28 -0800

    Fixed Settings option
    The Settings menu could not be open because of how the setting was trying to reference the root of the GUI since the splitting of the files. Now this should reference the master of the GUI (which is basically the same thing as the root) and added importing thing to make it work.

--- guilib/application.py
+++ guilib/application.py
@@ -1,13 +1,15 @@
-import tkinter as tk
-from guilib.numberedtext import NumberedText
+import sys
 import os
+import threading
+import tkinter as tk
+from tkinter import ttk
 from tkinter import scrolledtext
+from tkinter import filedialog as fd
+from guilib.numberedtext import NumberedText
 from interpreter import interpret
 from datetime import datetime
-from tkinter import filedialog as fd
-import sys
 from importlib import import_module
-import threading
+
 
 class Application(tk.Frame):
    def __init__(self, master=None):
@@ -117,7 +119,7 @@ class Application(tk.Frame):
         parentHeight = int(parentStats[0].split('x')[1])
 
         #If maximized window, will assume parent is at x = 0, y = 0
-        if root.state() == "zoomed":
+        if self.master.state() == "zoomed":
           parentStats[1] = "0"
           parentStats[2] = "0"
 
@@ -197,19 +199,7 @@ class Application(tk.Frame):
    def create_console_window(self):
       self.console = tk.scrolledtext.ScrolledText(self.bottomframe, state = "disabled", cursor = "arrow", height = 6)
       self.console.pack(padx = 5, pady = 10, expand = True, fill = "x")
-    
-   # Functions that clear text
-   def clear_input_window(self):
-      self.input.text.delete('1.0', tk.END)
-   def clear_output_window(self):
-      self.output.text.config(state="normal")
-      self.output.text.delete('1.0', tk.END)
-      self.output.text.config(state="disable")
-   def clear_console_window(self):    
-      self.console.config(state="normal")
-      self.console.delete('1.0', tk.END)
-      self.console.config(state="disabled")
-      
+
    # saves into pseudo file. Calls a print to console and python output
    def interpreter_func(self):
       # Maybe have a toggle to enable library updates in here so that when
@@ -388,14 +378,7 @@ class Application(tk.Frame):
       thememenu.add_checkbutton(label="Normal", onvalue=1, offvalue=0, variable=self.normal_theme, command=lambda: self.set_theme("normal"))
       thememenu.add_checkbutton(label="Dark", onvalue=1, offvalue=0, variable=self.dark_theme, command=lambda: self.set_theme("dark"))
       menubar.add_cascade(label="Theme", menu=thememenu)
-      
-      # clear menu
-      clearmenu = tk.Menu(menubar, tearoff=0)
-      clearmenu.add_command(label="Console", command=lambda: self.clear_console_window(), accelerator="F8")
-      clearmenu.add_command(label="Input", command=lambda: self.clear_input_window(), accelerator="F9")
-      clearmenu.add_command(
-          label="Output", command=lambda: self.clear_output_window(), accelerator="F10")
-      
+        
       # Specific style configurations, currently a child of the theme menu.
       configMenu = tk.Menu(thememenu, tearoff=0)
       thememenu.add_cascade(label="Config", menu=configMenu)
