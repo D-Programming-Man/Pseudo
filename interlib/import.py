fa66fac5deeadbd3e6f8bcb551f51e1cc5bfcf00
D-Programming-Man <40450924+D-Programming-Man@users.noreply.github.com>
2020-12-05 16:06:13 -0800

    Import files from same directory
    This makes it so that when a user is importing files, it must be in the same directory of the main pseudo file rather than the program.

--- interlib/import.py
+++ interlib/import.py
@@ -35,6 +35,8 @@ def handler(interpret_state):
   indent = interpret_state["pseudo_indent"] + interpret_state["indent"]
   py_lines = interpret_state["py_lines"]
   import_queue = interpret_state["import_queue"]
+  pseudo_filepath = interpret_state["pseudo_filepath"]
+  pseudo_file = interpret_state["pseudo_file"]
   
   if len(line_list) > 2:
     print("Cannot import files with spaces in the names.")
@@ -44,22 +46,25 @@ def handler(interpret_state):
   # Find the file in the same directory as the main pseudo file
   word_pos = 1
   file_list_pos = 0
-  pseudo_files = glob.glob("./*.pseudo")
+  pseudo_files = glob.glob(pseudo_filepath + "/*.pseudo")
+  #print(pseudo_files)
   for file in pseudo_files:
-    file = file[2:-7]
+    file = file.split("\\")[-1][0:-7]
+    #file = file[2:-7]
+    #print(file)
     if file == line_list[1]:
       break;
     file_list_pos += 1
 
   pseudo_file_name = ""
-  pseudo_file = ""
+  pseudo_filepath = ""
   pseudo_file_py = ""
   try:
-    pseudo_file_name = pseudo_files[file_list_pos][2:-7]
-    pseudo_file = pseudo_files[file_list_pos][2:]
-    pseudo_file_py = pseudo_file_name + ".py"
+    pseudo_file_name = pseudo_files[file_list_pos].split("\\")[-1]
+    pseudo_filepath = pseudo_files[file_list_pos]
+    pseudo_file_py = pseudo_files[file_list_pos][0:-7] + ".py"
   except:
-    print("There is no pseudo file named \"" + line_list[1] + "\"")
+    print("Error: There is no pseudo file named \"" + line_list[1] + ".pseudo\" within the same directory as \"" + pseudo_file + "\"")
     print_line(line_numb, line_list)
     return False
 
@@ -76,7 +81,7 @@ def handler(interpret_state):
     return False
 
   # Parse all code in the importing pseudo file
-  sub_interpret_state = import_interpret(pseudo_file, pseudo_file_py, interpret_state["keyword_dict"], import_queue)
+  sub_interpret_state = import_interpret(pseudo_filepath, pseudo_file_py, interpret_state["keyword_dict"], import_queue)
   if sub_interpret_state["parse_success"]:
     import_queue.pop()
     
@@ -87,7 +92,7 @@ def handler(interpret_state):
     for var in sub_interpret_state["all_variables"]:
       if sub_interpret_state["all_variables"][var]["data_type"] == "function":
         if var not in all_variables:
-          if sub_interpret_state["all_variables"][var]["source"] == pseudo_file:
+          if sub_interpret_state["all_variables"][var]["source"] != pseudo_file:
             contains_functions = True
             interpret_state["all_variables"][var] = sub_interpret_state["all_variables"][var]
             py_lines.appendleft("from " + pseudo_file_name + " import " + var + "\n")
